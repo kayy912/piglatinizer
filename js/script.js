736e16c12c900ebe632af13515cfe43369d0443d
kayy912 <kwanasiacole912@gmail.com>
2019-10-29 22:23:16 +0000

    Message

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
 
+function isVowel(x) {
+         return ("aeiouAEIOU".indexOf(x) !== -1);
+}
+$("#button").click(function(){
+    let word = $(".input").val();
+    let first = word.charAt(0);
+    if(isVowel(first)){
+        word = word + "ay";
+     }
+     else{
+
+     }
+ $(".output").text(word);
+});
 
 
-  
-  
-  
-  
+
+
+
+
 });
